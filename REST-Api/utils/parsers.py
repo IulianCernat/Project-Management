42ebeb8aaece6670ab80d792c23f6fe72b6871ed
IulianCernat <iulian.cernat98@yahoo.com>
2021-05-12 17:12:58 +0300

    +Add url parsers

--- REST-Api/utils/parsers.py
+++ REST-Api/utils/parsers.py
@@ -7,3 +7,9 @@ projects_sorting_arguments = api.parser()
 projects_sorting_arguments.add_argument('user_id', type=int, location='args', required=True)
 projects_sorting_arguments.add_argument('user_type', type=str, choices=['productOwner', 'scrumMaster', 'developer'],
                                         location='args', required=True)
+
+teams_members_filtering_args = api.parser()
+teams_members_filtering_args.add_argument('team_id', type=int, location='args', required=True)
+
+user_filtering_args = api.parser()
+user_filtering_args.add_argument('search', type=str, location='args', required=True)
