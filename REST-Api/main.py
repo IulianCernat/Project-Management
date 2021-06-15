9fa803067f6a55783947547322b884689d7304bb
IulianCernat <iulian.cernat98@yahoo.com>
2021-06-15 11:16:19 +0300

    + Clean code

--- REST-Api/main.py
+++ REST-Api/main.py
@@ -12,51 +12,39 @@ from endpoints.sprints_endpoint import sprints_namespace
 
 from flask_cors import CORS
 
-app = Flask(__name__)
 
-log = logging.getLogger(__name__)
+class FlaskApp(Flask):
+    def __init__(self, import_name):
+        super().__init__(import_name)
+        self.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
+        self.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
+        self.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTX_SWAGGER_UI_DOC_EXPANSION
+        self.config['RESTX_VALIDATE'] = settings.RESTX_VALIDATE
+        self.config['RESTX_MASK_SWAGGER'] = settings.RESTX_MASK_SWAGGER
+        self.config['ERROR_404_HELP'] = settings.RESTX_ERROR_404_HELP
 
+        blueprint = Blueprint('api', __name__, url_prefix='/api')
+        api.init_app(blueprint)
 
-def configure_app(flask_app):
-    # flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
-    flask_app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
-    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
-    flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTX_SWAGGER_UI_DOC_EXPANSION
-    flask_app.config['RESTX_VALIDATE'] = settings.RESTX_VALIDATE
-    flask_app.config['RESTX_MASK_SWAGGER'] = settings.RESTX_MASK_SWAGGER
-    flask_app.config['ERROR_404_HELP'] = settings.RESTX_ERROR_404_HELP
+        api.add_namespace(users_namespace)
+        api.add_namespace(projects_namespace)
+        api.add_namespace(teams_namespace)
+        api.add_namespace(teams_members_namespace)
+        api.add_namespace(issues_namespace)
+        api.add_namespace(sprints_namespace)
 
-    # flask_app.config['PROPAGATE_EXCEPTIONS'] = True
+        self.register_blueprint(blueprint)
 
+        db.init_app(self)
+        self.app_context().push()
 
-def initialize_app(flask_app):
-    configure_app(flask_app)
-    blueprint = Blueprint('api', __name__, url_prefix='/api')
-    api.init_app(blueprint)
+        with self.app_context():
+            db.create_all()  # Create database tables for our data models
 
-    api.add_namespace(users_namespace)
-    api.add_namespace(projects_namespace)
-    api.add_namespace(teams_namespace)
-    api.add_namespace(teams_members_namespace)
-    api.add_namespace(issues_namespace)
-    api.add_namespace(sprints_namespace)
-
-    flask_app.register_blueprint(blueprint)
-
-    db.init_app(flask_app)
-    flask_app.app_context().push()
-
-    with flask_app.app_context():
-        db.create_all()  # Create database tables for our data models
-
-    CORS(flask_app)  # for development purpose, CORS is set to everyone
+        CORS(self)
 
 
-def main():
-    initialize_app(app)
-    log.info('>>>>> Starting development server at http://{}/api/ <<<<<'.format(app.config['SERVER_NAME']))
-    app.run(host='0.0.0.0', debug=settings.FLASK_DEBUG)
-
+app = FlaskApp(__name__)
 
 if __name__ == "__main__":
-    main()
+    app.run(host="0.0.0.0", debug=settings.FLASK_DEBUG)
--- REST-Api/settings.py
+++ REST-Api/settings.py
@@ -1,6 +1,6 @@
 # Flask settings
 # FLASK_SERVER_NAME = 'localhost:3000'
-FLASK_DEBUG = True # Do not use debug mode in production
+FLASK_DEBUG = False# Do not use debug mode in production
 
 # Flask-Restplus settings
 RESTX_SWAGGER_UI_DOC_EXPANSION = 'list'
@@ -9,5 +9,7 @@ RESTX_MASK_SWAGGER = False
 RESTX_ERROR_404_HELP = False
 
 # SQLAlchemy settings
+# For development
 SQLALCHEMY_DATABASE_URI = 'mariadb+pymysql://root:root@localhost:3306/projectsmanager'
+
 SQLALCHEMY_TRACK_MODIFICATIONS = True
